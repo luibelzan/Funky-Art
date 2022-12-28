cf03722813b8b23923a7fd961d4a16eca4aed236
luibelzan <luismi_madrilista@hotmail.com>
2022-12-28 17:37:24 +0100

    MyNFT contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require("Migrations");
-const TodoList = artifacts.require("TodoList");
+//const TodoList = artifacts.require("TodoList");
+const MyNFT = artifacts.require("MyNFT");
 
 module.exports = function (deployer) {
     deployer.deploy(Migrations);
-    deployer.deploy(TodoList);
+    //deployer.deploy(TodoList);
+    deployer.deploy(MyNFT);
 };
